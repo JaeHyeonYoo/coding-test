f07f1ed6247c2e25453684eba53fd6ec1d84f907
JaeHyeonYoo <dbwogus308@naver.com>
2022-11-21 23:09:11 +0900

    음수구하기

--- src/Test5.java
+++ src/Test5.java
@@ -4,12 +4,13 @@ public class Test5 {
 //        int[] absolute = {1,2,3};
 //        boolean[] sign = {false,false,true};
 //        solution.solution(absolute,sign);
+
     }
 }
-//class Solution5 {
-//    public int solution(int[] absolutes, boolean[] signs) {
+class Solution5 {
+    public int solution(int[] absolutes, boolean[] signs) {
 //        int answer = 123456789;
 //
-//        return answer;
-//    }
-//}
+        return 0;
+    }
+}
