d94fb96c9562c645582e5a7fdd3b2aac05e97f98
JaeHyeonYoo <dbwogus308@naver.com>
2022-11-29 18:02:11 +0900

    삼총사

---
+++ src/Test6.java
@@ -0,0 +1,13 @@
+public class Test6 {
+    public static void main(String[] args) {
+    Solution6 solution6 = new Solution6();
+    int[] array = {-2, 3, 0, 2, -5};
+    solution6.solution(array);
+    }
+}
+class Solution6 {
+    public int solution(int[] number) {
+        int answer = 0;
+        return answer;
+    }
+}
