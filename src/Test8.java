439f586ce0638b0df7a63d09f80d91421786b82b
JaeHyeonYoo <dbwogus308@naver.com>
2022-12-12 15:38:33 +0900

    편지

--- src/Test8.java
+++ src/Test8.java
@@ -5,6 +5,17 @@ public class Test8 {
         System.out.println(solution8.solution8("happy birthday!"));
     }
 }
+//머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며,
+// 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로길이를 return 하도록
+// solution 함수를 완성해주세요.
+
+//입출력 예
+//message	         result
+
+//"happy birthday!"	  30
+
+//"I love you~"	      22
+
 class Solution8 {
     public int solution8(String message) {
         int answer = 1;
