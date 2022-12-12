fb4ee7b74e1b5de413687ffde08c3b3fc7001f2e
JaeHyeonYoo <dbwogus308@naver.com>
2022-12-12 15:39:44 +0900

    제곱수 판별하기

--- src/Test9.java
+++ src/Test9.java
@@ -4,6 +4,14 @@ public class Test9 {
         System.out.println(solution9.solution(144));
     }
 }
+//어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때,
+// n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+
+//입출력 예
+//n	    result
+//144	1
+//976	2
+
 class Solution9 {
     public int solution(int n) {
         if(Math.sqrt(n)%1 == 0) return 1;
